56fdc2a088b1980678cb3513aa444e8445950e95
Sinisa Abramovic <sinisa.abramovich@gmail.com>
2018-11-09 22:02:14 +0100

    Project rearange

---
+++ 01 Classes and Objects/OOP Classes and Objects/OOP Classes and Objects/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  OOP Classes and Objects
+//
+//  Created by Sinisa Abramovic on 09/11/2018.
+//  Copyright © 2018 Sinisa Abramovic. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
