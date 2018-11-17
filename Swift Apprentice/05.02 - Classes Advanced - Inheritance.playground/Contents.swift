669d4a20280064be89b0df35608ddeea5f7e246b
Sinisa Abramovic <sinisa.abramovich@gmail.com>
2018-11-17 21:17:09 +0100

    no message

--- Swift Apprentice/05.02 - Classes Advanced - Inheritance.playground/Contents.swift
+++ Swift Apprentice/05.02 - Classes Advanced - Inheritance.playground/Contents.swift
@@ -61,5 +61,58 @@ class OboePlayer: BandMember {
 // ***************************************************
 
 let dick = OboePlayer(firstName: "Dick", lastName: "Tracy")
-print(dick.fullName)
+//print(dick.fullName)
 
+// ***************************************************
+// Primjer polimorfizma
+
+func phonebookName(_ person: Person) -> String {
+    return person.fullName
+}
+
+print(phonebookName(dick))
+print(phonebookName(john))
+print(phonebookName(jane))
+
+var hallMonitor = Student(firstName: "Bill", lastName: "Gates")
+
+// Provjera hijarahije klasa
+// as radi cast na odredjeni tip koji je poznat u trenutku kompajliranja
+// as? radi opcionalni downcast (na sub tip), ako ne uspije rezultat ce biti nil
+// as! forsirani downcast , ako ne uspije srusi program
+
+if let hallMonitor = hallMonitor as? BandMember {
+    print("This hall monitor is a band member and practices a least \(hallMonitor.minimumPracticeTime) hours per week.")
+}else {
+    print("This hall monitor is not a band member")
+}
+
+// Primjer da imamo dvije funkcije istog imena i istih imena parametara
+// ali drugacijeg tipa parametra
+func afterClassActivity(for student: Student) -> String {
+    return "Goes home!"
+}
+
+func afterClassActivity(for student: BandMember) -> String {
+    return "Goes to practice!"
+}
+
+print(afterClassActivity(for: dick))
+print(afterClassActivity(for: dick as Student))
+
+// U ovom primjeru SudentAthlete nadjacava recordGrade
+class StudentAthlete: Student {
+    var failedClasses: [Grade] = []
+    
+    override func recordGrade(_ grade: Grade) {
+        super.recordGrade(grade)
+        
+        if grade.letter == "F" {
+            failedClasses.append(grade)
+        }
+    }
+    
+    var isEligible: Bool {
+        return failedClasses.count < 3
+    }
+}
